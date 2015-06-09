b5cc1681791cb720efd365a19acff39f5e11042d
Cory Spitzer & Dan Minor <student@epicodus.com>
2015-06-09 11:29:25 -0700

    Link posts to their individual pages

--- router.js
+++ router.js
@@ -1,4 +1,5 @@
 Twatter.Router.map(function() {
   this.resource('posts', {path: '/'});
   this.resource('recent-comments');
+  this.resource('post', {path: 'posts/:post_id'})
 });
---
+++ routes/post.js
@@ -0,0 +1,5 @@
+Twatter.PostRoute = Ember.Route.extend({
+  model: function(params) {
+    return posts.findBy('id', params.post_id);
+  }
+});
