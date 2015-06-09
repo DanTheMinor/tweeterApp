8300e7f4ead80f013424203d742cc4afcd70e7bf
Cory Spitzer & Dan Minor <student@epicodus.com>
2015-06-09 09:20:10 -0700

    Add initial files

---
+++ store.js
@@ -0,0 +1,25 @@
+var posts = [
+  {
+    id: '1',
+    title: 'test',
+    body: 'some body text'
+  },
+  {
+    id: '2',
+    title: 'test2',
+    body: 'some body text2'
+  }
+]
+
+var comments = [
+  {
+    id: '1',
+    postId: '2',
+    text: 'What!?'
+  },
+  {
+    id: '2',
+    postId: '1',
+    text: 'Who!?'
+  }
+]
